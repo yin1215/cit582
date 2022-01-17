e5da7b9d8f4cc8807ba2f6fc12de2df85ef7c5b4
vegaschen <weijiac@seas.upenn.edu>
2022-01-17 18:33:03 -0500

    Create caesar.py

---
+++ caesar.py
@@ -0,0 +1,31 @@
+def encrypt(key, plaintext):
+    ciphertext = ""
+    incre = key % 26
+
+    for c in plaintext:
+        num = ord(c)
+
+        if num >=65 and num <=90:
+            num += incre
+            if num > 90:
+                num -= 26
+
+        ciphertext += chr(num)
+
+    return ciphertext
+
+
+def decrypt(key, ciphertext):
+    plaintext = ""
+    decre = key % 26
+
+    for c in ciphertext:
+        num = ord(c)
+
+        if num >= 65 and num <= 90:
+            num -= decre
+            if num < 90:
+                num += 26
+
+        plaintext += chr(num)
+    return plaintext
