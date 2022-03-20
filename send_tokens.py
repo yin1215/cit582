bb77a920b734dad8f39b0134589fc6128f824fc1
vegaschen <weijiac@seas.upenn.edu>
2022-03-20 00:19:41 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -23,7 +23,7 @@ def send_tokens( receiver_pk, tx_amount ):
     last_valid_round = params.last
 
     #Your code here
-    mnemonic_str = 'tonight people pizza apple turtle soup banana orange lemon tank skill light rifle peace place rock almost ring monkey rabbit mouse alien chicken tiger duck'
+    mnemonic_str = 'tonight people pizza apple turtle soup banana orange lemon tank skill light rifle peace place rocket almost ring monkey rabbit mouse alien chicken tiger duck'
     sk = mnemonic.to_private_key(mnemonic_str)
     sender_pk = mnemonic.to_public_key(mnemonic_str)
 
