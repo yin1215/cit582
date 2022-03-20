f599df511aa3a31282a83add863dcc5863e7c304
vegaschen <weijiac@seas.upenn.edu>
2022-03-19 23:46:07 -0400

    Create send_tokens.py

---
+++ send_tokens.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from algosdk.v2client import algod
+from algosdk import mnemonic
+from algosdk import transaction
+
+#Connect to Algorand node maintained by PureStake
+algod_address = "https://testnet-algorand.api.purestake.io/ps2"
+algod_token = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
+#algod_token = 'IwMysN3FSZ8zGVaQnoUIJ9RXolbQ5nRY62JRqF2H'
+headers = {
+   "X-API-Key": algod_token,
+}
+
+acl = algod.AlgodClient(algod_token, algod_address, headers)
+min_balance = 100000 #https://developer.algorand.org/docs/features/accounts/#minimum-balance
+
+def send_tokens( receiver_pk, tx_amount ):
+    params = acl.suggested_params()
+    gen_hash = params.gh
+    first_valid_round = params.first
+    tx_fee = params.min_fee
+    last_valid_round = params.last
+
+    #Your code here
+    mnemonic_str = 'Bwahaha tonight I dine on turtle soup'
+    sk = mnemonic.to_private_key(mnemonic_str)
+    sender_pk = mnemonic.to_public_key(mnemonic_str)
+
+    payment = transaction.PaymentTxn(sender= sender_pk, fee= tx_fee, first= first_valid_round, last= last_valid_round,
+                                     gh= gen_hash, receiver= receiver_pk, amt= tx_amount)
+
+    payment_signed = payment.sign(sk)
+    txid = acl.send_transaction(payment_signed)
+
+    return sender_pk, txid
+
+# Function from Algorand Inc.
+def wait_for_confirmation(client, txid):
+    """
+    Utility function to wait until the transaction is
+    confirmed before proceeding.
+    """
+    last_round = client.status().get('last-round')
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
+        print("Waiting for confirmation")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
+    return txinfo
+
